bad12b29a7025f352cb1ca4dd3b9f976c0558d1f
zmlovelin <656030719@qq.com>
2019-04-03 18:32:35 +0800

    edit table

--- src/app/business-module/table/table.component.ts
+++ src/app/business-module/table/table.component.ts
@@ -1,35 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TableConfig} from "../../shared-module/component/zm-table/tableConfig";
 
 @Component({
-  selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+    selector: 'app-table',
+    templateUrl: './table.component.html',
+    styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
-  public data = [];
-  constructor() { }
+    public data = [];
+    public tableConfig: TableConfig;
 
-  ngOnInit() {
-    this.data = [
-      {
-        key: '1',
-        name: 'John Brown',
-        age: 32,
-        address: 'New York No. 1 Lake Park'
-      },
-      {
-        key: '2',
-        name: 'Jim Green',
-        age: 42,
-        address: 'London No. 1 Lake Park'
-      },
-      {
-        key: '3',
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sidney No. 1 Lake Park'
-      }
-    ];
-  }
+    constructor() {
+    }
 
+    ngOnInit() {
+        this.initTableConfig();
+        this.data = [
+            {
+                id: '1',
+                name: 'John Brown',
+                age: 32,
+                address: 'New York No. 1 Lake Park'
+            },
+            {
+                id: '2',
+                name: 'Jim Green',
+                age: 42,
+                address: 'London No. 1 Lake Park'
+            },
+            {
+                id: '3',
+                name: 'Joe Black',
+                age: 32,
+                address: 'Sidney No. 1 Lake Park'
+            }
+        ];
+    }
+
+    private initTableConfig() {
+        this.tableConfig = {
+            columnConfig: [
+                // {type: 'select', fixedStyle: {fixedLeft: true, style: {left: '0px'}}, width: 62},
+                { // 巡检任务名称
+                    title: '巡检任务名称', key: 'inspectionTaskName', width: 200,
+                },
+                { // 巡检任务名称
+                    title: '巡检任务时间', key: 'inspectionTaskTime', width: 200,
+                },
+                { // 巡检任务名称
+                    title: '巡检任务周期', key: 'inspectionTask', width: 200,
+                }
+            ]
+        }
+    }
 }
